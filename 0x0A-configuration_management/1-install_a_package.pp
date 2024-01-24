337ce17dc351aa510bd8c5106fa2293ce39aef4a
She-ilah <sheilahmutale1@gmail.com>
2024-01-24 02:01:47 -0800

    task 0

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install a package puppet-lint -v 2.5.0
-exec { 'install_flask':
- command => 'pip3 install Flask==2.1.0',
- path	=> ['/usr/bin', '/bin'],
- unless => 'pip3 show Flask | grep Version | grep'
+# Installs puppet-lint
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
