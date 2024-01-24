0e63b686e56b198ca4a72284b7e41a322135508f
She-ilah <sheilahmutale1@gmail.com>
2024-01-24 01:42:47 -0800

    checks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
 # install a package puppet-lint -v 2.5.0
-package {'flask':
-	ensure	=> '2.1.0',
-	provider => 'pip3'
+exec { 'install_flask':
+ command => 'pip3 install Flask==2.1.0',
+ path	=> ['/usr/bin', '/bin'],
 }
