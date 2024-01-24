639e7be38ec7aa2bbd209cc3affad4a986be714d
She-ilah <sheilahmutale1@gmail.com>
2024-01-24 03:06:51 -0800

    advanced tasks

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Automation of tasks
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
